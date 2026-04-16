cfac5956ac2c5e01af3a725cb97b76448aaa302b
DanishAhmad111 <tamimqureshi865@gmail.com>
2026-04-16 14:49:39 +0530

    Updated eventTypes

--- frontend/src/types/eventType.ts
+++ frontend/src/types/eventType.ts
@@ -1,1 +1,11 @@
-export type {}
+export interface EventType {
+  id: string;
+  title: string;
+  description?: string | null;
+  slug: string;
+  duration: number;
+  isActive: boolean;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+}
